80312adc92635367947e35e608cde510daf76bf6
Josep Conejero <bermeu@gmail.com>
2022-01-24 13:50:21 +0100

    se crea index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+const patients = [
+  {
+    patient: {
+      name: "Luis",
+      age: 25,
+      sex: "M",
+    },
+    daysInHospital: 3,
+    diet: "Low fiber",
+  },
+  {
+    patient: {
+      name: "Marta",
+      age: 56,
+      sexo: "W",
+    },
+    daysInHospital: 5,
+    diet: "Diabetes",
+  },
+  {
+    patient: {
+      name: "Julia",
+      age: 38,
+      sexo: "W",
+    },
+    daysInHospital: 1,
+    diet: "Salt free",
+  },
+  {
+    patient: {
+      name: "Esteban",
+      age: 40,
+      sexo: "M",
+    },
+    daysInHospital: 3,
+    diet: "Diabetes",
+  },
+  {
+    patient: {
+      name: "Arturo",
+      age: 17,
+      sexo: "M",
+    },
+    daysInHospital: 2,
+    diet: "Low fiber",
+  },
+  {
+    patient: {
+      name: "Isabel",
+      age: 63,
+      sexo: "W",
+    },
+    daysInHospital: 6,
+    diet: "Salt free",
+  },
+];
---
+++ module.js
@@ -0,0 +1,20 @@
+
+function devolverDatos(pacientes){
+const datos = {};
+datos.nPatients =;
+datos.nAdults =;
+datos.nDiabeticMen =;
+datos.totalDaysInHospital =;
+datos.averageAgeWomen =;
+
+return datos;
+}
+
+/* {
+    nPatients: (número de pacientes),
+    nAdults: (número de pacientes mayores de edad),
+    nDiabeticMen: (número de pacientes hombres con dieta para diabetes),
+    totalDaysInHospital: (suma de los días de ingreso de todos los pacientes),
+    averageAgeWomen: (edad media de las pacientes mujeres)
+}
+ */
