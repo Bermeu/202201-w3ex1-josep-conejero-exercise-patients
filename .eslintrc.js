4b01a7d087cd4f56e612be96dca1c533fc23e435
Josep Conejero <bermeu@gmail.com>
2022-01-24 13:40:58 +0100

    se instalan todas las dependencias

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
+  "lines-between-class-members": [
+    "error",
+    "always",
+    { exceptAfterSingleLine: true },
+  ],
+};
